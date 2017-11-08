8772c360c2dd677a3535f840b1a2ea8cc7e67adf
Pete Waters <pete.d.waters@gmail.com>
2017-11-08 22:36:20 +0000

    Added tests for checking for listeners by event

--- tests/Unit/DispatcherTest.php
+++ tests/Unit/DispatcherTest.php
@@ -26,4 +26,29 @@ class DispatcherTest extends TestCase
 
         $this->assertCount(1, $dispatcher->getListeners()['shh']);
    }
+
+   /** @test */
+   public function can_get_listener_by_event_name()
+   {
+        $dispatcher = new Dispatcher;
+        $dispatcher->addListener('shh', new ListenerStub);
+
+        $this->assertCount(1, $dispatcher->getListenersByEventName('shh'));
+   }
+
+   /** @test */
+   public function returns_empty_array_if_no_listeners_found_for_event_name()
+   {
+        $dispatcher = new Dispatcher;
+
+        $this->assertCount(0, $dispatcher->getListenersByEventName('shh'));
+   }
+
+   /** @test */
+   public function can_check_for_listeners_for_event()
+   {
+        $dispatcher = new Dispatcher;
+
+        $this->assertFalse($dispatcher->hasListeners('shh'));
+   }
 }
