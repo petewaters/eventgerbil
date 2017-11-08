6002738313de07ec1385e6e147743bff437aa9be
Pete Waters <pete.d.waters@gmail.com>
2017-11-08 22:36:44 +0000

    Added methods for checking for listeners by event

--- app/Core/Events/Dispatcher.php
+++ app/Core/Events/Dispatcher.php
@@ -8,9 +8,19 @@ class Dispatcher
 {
     protected $listeners = [];
 
-    public function getListeners() : array
+    public function getListeners() :array
     {
         return $this->listeners;
+    }
+
+    public function hasListeners(string $name) 
+    {
+        return isset($this->listeners[$name]) ? true : false;
+    }
+
+    public function getListenersByEventName(string $name) : array
+    {
+        return $this->listeners[$name] ?? [];
     }
 
     public function addListener($name, Listener $listener)
